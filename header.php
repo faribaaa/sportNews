3d52037bfdf58f27119d698303124c3781dc5fcb
faribaaa <bestgirlhere@gmail.com>
2016-12-01 00:35:11 +0330

    Add files via upload

--- header.php
+++ header.php
@@ -10,19 +10,19 @@
 </head>
 <body>
 <nav class="navbar-default navbar-fixed-top">
-    <span class="col-xs-12"></span>
-    <div class="container-fluid">
+     <div class="container-fluid">
         <div class="navbar-header">
-            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
+            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" aria-expanded="false">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-
-            <div class="navbar-collapse collapse">
+                <span class="navbar-brand">SportNews</span>
+            </div>
+            <div class="collapse navbar-collapse " id="navbar1">
                 <ul class="nav navbar-nav ">
                     <li><a href="#"><span class="glyphicon glyphicon-home" id="txt-menu"> صفحه اصلی</span> </a> </li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"> ورود</span> </a> </li>
+                    <li><a href="#"><span class="fa fa-bars"> ورود</span> </a> </li>
                     <li><a href="#"><span class="glyphicon glyphicon-plus"> ثبت نام</span> </a> </li>
                     <li><a href="#">اخبار </a> </li>
                     <li><a href="#"> مطالب کاربران</a> </li>
@@ -30,42 +30,111 @@
                 </ul>
             </div>
         </div>
-    </div>
-</nav>
-<div class="container-fluid">
+   </nav>
+<div id="nav-fix-clear"></div>
+<div class="container">
     <div class="row">
         <div class="col-lg-12">
-            <div class="container-fluid">
+            <div class="slide_show">
                 <div id="top_slider" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
                         <li data-target="#top_slider" data-slide-to="0" class="active"></li>
                         <li data-target="#top_slider" data-slide-to="1" class="active"></li>
                         <li data-target="#top_slider" data-slide-to="2" class="active"></li>
-
                     </ol>
                     <div class="carousel-inner" role="listbox">
-                        <div class="item active"><img src="public/images/posts/1.jpg" alt="">
+                        <div class="item active">
+                            <img src="public/images/posts/1.jpg" alt="">
                             <div class="carousel-caption"></div>
                         </div>
-                        <div class="item"><img src="public/images/posts/1.jpg" alt="">
+                        <div class="item">
+                            <img src="public/images/posts/1.jpg" alt="">
                             <div class="carousel-caption"></div>
                         </div>
-                        <div class="item"><img src="public/images/posts/1.jpg">
+                        <div class="item">
+                            <img src="public/images/posts/1.jpg">
                             <div class="carousel-caption"></div>
                         </div>
                     </div>
-                    <a class="left carousel-control" href="#top_slider" role="button" data-slide="pre">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">previ</span>
-                    </a>
-                    <a class="right carousel-control" href="#top_slider" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">next</span>
-                    </a>
+
                 </div>
             </div>
-
         </div>
     </div>
 </div>
+<div class="row" id="menu2">
+    <div class="col-lg-12 col-sm-9">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-main">
+                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar2" aria-expanded="false">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                    <div class="collapse navbar-collapse" id="navbar2">
+                        <ul class="nav navbar-nav navbar-main">
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">مرکز خبر<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li class="dropdown-header">سایت</li>
+                                <li class="dropdown-header">تلوزیون</li>
+                                <li class="dropdown-header">رادیو</li>
+                            </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">لیگ ها<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">داخلی</li>
+                                    <li class="dropdown-header">خارجی</li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">باشگاها<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">داخلی</li>
+                                    <li class="dropdown-header">خارجی</li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">تیم ها<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">داخلی</li>
+                                    <li class="dropdown-header">خارجی</li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">بازیکنان فوتبال<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">داخلی</li>
+                                    <li class="dropdown-header">خارجی</li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">استدیوم ها<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">داخلی</li>
+                                    <li class="dropdown-header">خارجی</li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">مقالات<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">داخلی</li>
+                                    <li class="dropdown-header">خارجی</li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">عکس ها<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">داخلی</li>
+                                    <li class="dropdown-header">خارجی</li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">ویدئوها<b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">داخلی</li>
+                                    <li class="dropdown-header">خارجی</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+            </div>
+        </nav>
+    </div>
+</div>
+
 </body>
