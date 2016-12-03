da58d1bc78fe34f15da0d0f048e3ab90422c631d
faribaaa <bestgirlhere@gmail.com>
2016-12-03 23:27:53 +0330

    sportnews

--- header.php
+++ header.php
@@ -1,282 +1,47 @@
-<<<<<<< HEAD
-<html>
-<head>
-    <title>sportNews</title>
-    <link href="public/css/bootstrap.min.css" rel="stylesheet" >
-    <link href="public/css/stylesheet.css" rel="stylesheet" type="text/css">
-    <link href="public/css/bootstrap-rtl.min.css" rel="stylesheet">
-    <script src="public/js/jquery-3.0.0.min.js"></script>
-    <script src="public/js/main.js"></script>
-    <script src="public/js/bootstrap.min.js" type="text/javascript" ></script>
-</head>
-<body>
-<nav class="navbar-default navbar-fixed-top">
-     <div class="container-fluid">
-        <div class="navbar-header">
-            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" aria-expanded="false">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-                <span class="navbar-brand">SportNews</span>
-            </div>
-            <div class="collapse navbar-collapse " id="navbar1">
-                <ul class="nav navbar-nav ">
-                    <li><a href="#"><span class="glyphicon glyphicon-home" id="txt-menu"> صفحه اصلی</span> </a> </li>
-                    <li><a href="#"><span class="fa fa-bars"> ورود</span> </a> </li>
-                    <li><a href="#"><span class="glyphicon glyphicon-plus"> ثبت نام</span> </a> </li>
-                    <li><a href="#">اخبار </a> </li>
-                    <li><a href="#"> مطالب کاربران</a> </li>
-                    <li><a href="#">نتایج مسابقات </a> </li>
-                </ul>
-            </div>
-        </div>
-   </nav>
-<div id="nav-fix-clear"></div>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="slide_show">
-                <div id="top_slider" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#top_slider" data-slide-to="0" class="active"></li>
-                        <li data-target="#top_slider" data-slide-to="1" class="active"></li>
-                        <li data-target="#top_slider" data-slide-to="2" class="active"></li>
-                    </ol>
-                    <div class="carousel-inner" role="listbox">
-                        <div class="item active">
-                            <img src="public/images/posts/1.jpg" alt="">
-                            <div class="carousel-caption"></div>
-                        </div>
-                        <div class="item">
-                            <img src="public/images/posts/1.jpg" alt="">
-                            <div class="carousel-caption"></div>
-                        </div>
-                        <div class="item">
-                            <img src="public/images/posts/1.jpg">
-                            <div class="carousel-caption"></div>
-                        </div>
-                    </div>
 
-                </div>
+
+
+<div class="top_menu" id="menu1">
+    <div class=" container header">
+        <div class="logo_right">
+            <div class="img">
+                <img src="public/images/icons/lg.png">
             </div>
+        <ul class="nav navbar-nav">
+            <li><a href="#">سیما</a> </li>
+            <li><a href="#">صدا</a> </li>
+            <li><a href="#">معارف</a> </li>
+            <li><a href="#">ورزش</a> </li>
+        </ul>
+    </div>
+        <div class="logo_left">
+            <img src="public/images/icons/lg1.png">
+            <img src="public/images/icons/lg2.png">
+            <span>SportNews</span>
         </div>
     </div>
 </div>
-<div class="row" id="menu2">
-    <div class="col-lg-12 col-sm-9">
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-main">
-                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar2" aria-expanded="false">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                    <div class="collapse navbar-collapse" id="navbar2">
-                        <ul class="nav navbar-nav navbar-main">
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">مرکز خبر<b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-header">سایت</li>
-                                <li class="dropdown-header">تلوزیون</li>
-                                <li class="dropdown-header">رادیو</li>
-                            </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">لیگ ها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">باشگاها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">تیم ها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">بازیکنان فوتبال<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">استدیوم ها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">مقالات<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">عکس ها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">ویدئوها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-            </div>
-        </nav>
-    </div>
-</div>
 
-=======
-<html>
-<head>
-    <title>sportNews</title>
-    <link href="public/css/bootstrap.min.css" rel="stylesheet" >
-    <link href="public/css/stylesheet.css" rel="stylesheet" type="text/css">
-    <link href="public/css/bootstrap-rtl.min.css" rel="stylesheet">
-    <script src="public/js/jquery-3.0.0.min.js"></script>
-    <script src="public/js/main.js"></script>
-    <script src="public/js/bootstrap.min.js" type="text/javascript" ></script>
-</head>
-<body>
-<nav class="navbar-default navbar-fixed-top">
-     <div class="container-fluid">
-        <div class="navbar-header">
-            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" aria-expanded="false">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-                <span class="navbar-brand">SportNews</span>
-            </div>
-            <div class="collapse navbar-collapse " id="navbar1">
-                <ul class="nav navbar-nav ">
-                    <li><a href="#"><span class="glyphicon glyphicon-home" id="txt-menu"> صفحه اصلی</span> </a> </li>
-                    <li><a href="#"><span class="fa fa-bars"> ورود</span> </a> </li>
-                    <li><a href="#"><span class="glyphicon glyphicon-plus"> ثبت نام</span> </a> </li>
-                    <li><a href="#">اخبار </a> </li>
-                    <li><a href="#"> مطالب کاربران</a> </li>
-                    <li><a href="#">نتایج مسابقات </a> </li>
-                </ul>
+<div class="top_head" id="menu2">
+    <div class=" container header1">
+        <div class="header_right">
+            <div class="img">
+                <img src="public/images/icons/Untitled.jpg">
             </div>
         </div>
-   </nav>
-<div id="nav-fix-clear"></div>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="slide_show">
-                <div id="top_slider" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#top_slider" data-slide-to="0" class="active"></li>
-                        <li data-target="#top_slider" data-slide-to="1" class="active"></li>
-                        <li data-target="#top_slider" data-slide-to="2" class="active"></li>
-                    </ol>
-                    <div class="carousel-inner" role="listbox">
-                        <div class="item active">
-                            <img src="public/images/posts/1.jpg" alt="">
-                            <div class="carousel-caption"></div>
-                        </div>
-                        <div class="item">
-                            <img src="public/images/posts/1.jpg" alt="">
-                            <div class="carousel-caption"></div>
-                        </div>
-                        <div class="item">
-                            <img src="public/images/posts/1.jpg">
-                            <div class="carousel-caption"></div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+        <div class="header_main">
+            <ul class="nav navbar-nav" id="txt_header">
+                <li><a href="#">برنامه ها</a> </li>
+                <li><a href="#">اخبار</a> </li>
+                <li><a href="#">نتایج بازی ها</a> </li>
+                <li><a href="#">نظر سنجی </a> </li>
+                <li><a href="#">باشگاه همراهان </a> </li>
+                <li><a href="#">ورزش در سیما </a> </li>
+            </ul>
+        </div>
+        <div class="header_left">
+            <span>SportNews</span>
         </div>
     </div>
 </div>
-<div class="row" id="menu2">
-    <div class="col-lg-12 col-sm-9">
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-main">
-                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar2" aria-expanded="false">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                    <div class="collapse navbar-collapse" id="navbar2">
-                        <ul class="nav navbar-nav navbar-main">
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">مرکز خبر<b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-header">سایت</li>
-                                <li class="dropdown-header">تلوزیون</li>
-                                <li class="dropdown-header">رادیو</li>
-                            </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">لیگ ها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">باشگاها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">تیم ها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">بازیکنان فوتبال<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">استدیوم ها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">مقالات<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">عکس ها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                            <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">ویدئوها<b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-header">داخلی</li>
-                                    <li class="dropdown-header">خارجی</li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-            </div>
-        </nav>
-    </div>
-</div>
 
->>>>>>> origin/master
-</body>
--- index.php
+++ index.php
@@ -5,9 +5,79 @@
 //var_dump($allPost->getAll());
 ?>
 
+<html>
+<head>
+    <title>sportNews</title>
+    <link href="public/css/bootstrap.min.css" rel="stylesheet" >
+    <link href="public/css/stylesheet.css" rel="stylesheet" type="text/css">
+    <link href="public/css/bootstrap-rtl.min.css" rel="stylesheet">
+</head>
+<body>
 <?php
 include "header.php";
 include "footer.php";
 ?>
+<script src="public/js/jquery-3.0.0.min.js"></script>
+<script src="public/js/main.js"></script>
+<script src="public/js/bootstrap.min.js" type="text/javascript" ></script>
+
+<div id="nav-fix-clear"></div>
+<div class="row">
+    <div class="col-lg-3">
+        <div class="box_main_right">
+            <ul class="nav navbar-default" id="txt_main_right">
+                <li><a href="#"> جدول پخش یرنامه ها</a> </li>
+                <hr>
+                <li><a href="#">  برنامه های تخصصی</a> </li>
+                <hr>
+                <li><a href="#"> ویدوئوها</a> </li>
+                <hr>
+                <li><a href="#">  راه ها ی ارتباطی</a> </li>
+            </ul>
+
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="box_main_center">
+            <div class="slide_show">
+                <div id="top_slider" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#top_slider" data-slide-to="0" class="active"></li>
+                        <li data-target="#top_slider" data-slide-to="1" class="active"></li>
+                        <li data-target="#top_slider" data-slide-to="2" class="active"></li>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="public/images/avatar/ronaldo.jpg" alt="">
+                            <div class="carousel-caption"></div>
+                        </div>
+                        <div class="item">
+                            <img src="public/images/avatar/zinedine.jpg" alt="">
+                            <div class="carousel-caption"></div>
+                        </div>
+                        <div class="item">
+                            <img src="public/images/avatar/pele.jpg" alt="">
+                            <div class="carousel-caption"></div>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="box_main_left">
+            <ul class="nav navbar-default" id="txt_main_left">
+                <li><a href="#">رویدادهای ورزشی</a> </li>
+                <hr>
+                <li><a href="#">بسکتبال حرفه ای</a> </li>
+                <hr>
+                <li><a href="#">لیگ بسکتبال</a> </li>
+                <hr>
+                <li><a href="#">فوتبال انگلستان</a> </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div id="nav-fix-clear"></div>
 </body>
 </html>
